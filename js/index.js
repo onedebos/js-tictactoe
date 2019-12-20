6c24a77c93d60ca1edab43325b379bbfd846484d
Adebola <onedebos@gmail.com>
2019-12-20 17:06:48 +0100

    fix lint errors

--- js/index.js
+++ js/index.js
@@ -1,79 +1,77 @@
-//gameplay
+/* eslint-disable radix */
+/* eslint-disable no-use-before-define */
+//  gameplay
 const Game = (p1, p2, turnText, counter, space) => {
-  const form = document.querySelector(".form");
-  Game.space = document.querySelector(".game-board");
-  
-  Game.turnText = document.querySelector(".turn-text");
+  const form = document.querySelector('.form');
+  Game.space = document.querySelector('.game-board');
+
+  Game.turnText = document.querySelector('.turn-text');
 
   const start = () => {
     GameBoard.resetBoard();
     const clearPlayerNames = () => {
-      let fieldsToClear = document.getElementsByClassName("input");
-      let fieldsArr = [...fieldsToClear];
-      fieldsArr.forEach(item => {
-        item.value = "";
+      const fieldsToClear = document.getElementsByClassName('input');
+      const fieldsArr = [...fieldsToClear];
+      fieldsArr.forEach(() => {
+        fieldsArr.value = '';
       });
     };
     const getPlayerNames = () => {
-      let player1 = document.getElementById("name-1").value;
-      let player2 = document.getElementById("name-2").value;
+      const player1 = document.getElementById('name-1').value;
+      const player2 = document.getElementById('name-2').value;
 
-      if (player1 !== "" && player2 !== "") {
-        Game.p1 = Player(player1, "X");
-        Game.p2 = Player(player2, "O");
+      if (player1 !== '' && player2 !== '') {
+        Game.p1 = Player(player1, 'X');
+        Game.p2 = Player(player2, 'O');
         GameBoard.showBoard();
         runGame.hideForm();
         runGame.addXorO();
         GameBoard.restartBtn();
         clearPlayerNames();
       } else {
-        const formContainer = document.querySelector(".box-1");
-        const div = document.createElement("div");
-        div.classList.add("notification");
-        div.classList.add("is-danger");
+        const formContainer = document.querySelector('.box-1');
+        const div = document.createElement('div');
+        div.classList.add('notification');
+        div.classList.add('is-danger');
         div.innerHTML = `Enter player names to start game`;
         formContainer.appendChild(div);
-        setTimeout(
-          () => document.querySelector(".notification").remove(),
-          3000
-        );
+        setTimeout(() => document.querySelector('.notification').remove(), 3000);
       }
     };
 
     GameBoard.hideBoard();
     runGame.showForm();
-    const startButton = document.querySelector(".start-now");
-    startButton.addEventListener("click", getPlayerNames);
+    const startButton = document.querySelector('.start-now');
+    startButton.addEventListener('click', getPlayerNames);
   };
 
   const showForm = () => {
-    form.classList.add("show");
+    form.classList.add('show');
   };
 
   const hideForm = () => {
-    form.style.display = "none";
+    form.style.display = 'none';
   };
 
-  //add X or O to the board
+  //  add X or O to the board
   const addXorO = () => {
     Game.counter = 1;
     GameBoard.oMoves = [];
     GameBoard.xMoves = [];
 
-
-    Game.space.addEventListener("click", event => {
+    Game.space.addEventListener('click', event => {
       if (event.target.innerHTML.length === 0) {
         if (Game.counter % 2 === 0) {
-          GameBoard.oMoves.push(parseInt(event.target.getAttribute("data-num")));
-          event.target.innerHTML = "O";
-          event.target.setAttribute("class", "O");
+          GameBoard.oMoves.push(parseInt(event.target.getAttribute('data-num')));
+          event.target.innerHTML = 'O';
+          event.target.setAttribute('class', 'O');
           Game.turnText.innerHTML = `It is ${Game.p1.name}'s turn`;
           Game.counter++;
           GameBoard.checkForWin(GameBoard.oMoves, Game.p2.name, 'danger');
         } else {
-          GameBoard.xMoves.push(parseInt(event.target.getAttribute("data-num")));
-          event.target.innerHTML = "X";
-          event.target.setAttribute("class", "X");
+          GameBoard.xMoves.push(parseInt(event.target.getAttribute('data-num')));
+          event.target.innerHTML = 'X';
+          event.target.setAttribute('class', 'X');
           Game.turnText.innerHTML = `It is ${Game.p2.name}'s turn`;
           Game.counter++;
           GameBoard.checkForWin(GameBoard.xMoves, Game.p1.name, 'warning');
@@ -87,12 +85,22 @@ const Game = (p1, p2, turnText, counter, space) => {
     });
   };
 
-  return { showForm, hideForm, start, addXorO, p1,p2,turnText, counter, space };
+  return {
+    showForm,
+    hideForm,
+    start,
+    addXorO,
+    p1,
+    p2,
+    turnText,
+    counter,
+    space
+  };
 };
 
-//GameBoard
+//  GameBoard
 const GameBoard = ((oMoves, xMoves) => {
-   let winCounter = 0;
+  let winCounter = 0;
   const winningCombinations = [
     [0, 1, 2],
     [3, 4, 5],
@@ -104,11 +112,9 @@ const GameBoard = ((oMoves, xMoves) => {
     [2, 4, 6]
   ];
 
-
-
   const checkForWin = (movesArray, name, color) => {
     // loop over the first array of winning combinations
-    for (let i = 0; i < winningCombinations.length; i++) {
+    for (let i = 0; i < winningCombinations.length; i += 1) {
       // reset the winCounter each time!
       winCounter = 0;
       // loop over each individual array
@@ -122,52 +128,59 @@ const GameBoard = ((oMoves, xMoves) => {
           // const turnText = document.querySelector(".turn-text");
           Game.turnText.innerHTML = `Game over, ${name} wins!.`;
           Game.turnText.classList.add(`is-${color}`);
-          Game.turnText.classList.add("notification");
+          Game.turnText.classList.add('notification');
           setTimeout(GameBoard.resetBoard, 1500);
         }
       }
     }
   };
 
-  const board = document.querySelector(".game-board");
+  const board = document.querySelector('.game-board');
   const resetBtn = document.querySelector('.reset-btn');
   const hideBoard = () => {
-    board.classList.add("hide");
+    board.classList.add('hide');
     resetBtn.style.display = 'none';
   };
   const showBoard = () => {
-    board.classList.remove("hide");
+    board.classList.remove('hide');
     resetBtn.style.display = 'block';
   };
 
-  const restartBtn = () =>{ 
+  const restartBtn = () => {
     resetBtn.addEventListener('click', resetBoard);
-  }
+  };
 
   const resetBoard = () => {
     Game.counter = 1;
-    Game.turnText.classList.remove("is-danger");
-    Game.turnText.classList.remove("notification");
-    Game.turnText.innerHTML = `Let's play again!`
-    let cells = document.getElementsByTagName("td");
-    let cellsArr = [...cells];
+    Game.turnText.classList.remove('is-danger');
+    Game.turnText.classList.remove('notification');
+    Game.turnText.innerHTML = `Let's play again!`;
+    const cells = document.getElementsByTagName('td');
+    const cellsArr = [...cells];
     cellsArr.forEach(cell => {
-      cell.innerHTML = "";
-      cell.setAttribute("class", "clear");
+      cell.innerHTML = '';
+      cell.setAttribute('class', 'clear');
     });
 
     GameBoard.xMoves = [];
     GameBoard.oMoves = [];
-    Game.space.removeEventListener('click', ()=>{});
- 
+    Game.space.removeEventListener('click', () => {});
   };
-  return { hideBoard, resetBoard, showBoard, checkForWin, restartBtn, oMoves, xMoves };
+  return {
+    hideBoard,
+    resetBoard,
+    showBoard,
+    checkForWin,
+    restartBtn,
+    oMoves,
+    xMoves
+  };
 })();
 
 const Player = (name, symbol) => {
   return { name, symbol };
 };
 
-//gameplay
+//  gameplay
 const runGame = Game();
 runGame.start();
